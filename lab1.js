9f7b24ad31b6d2b6fea98981bf44cc08c3e7ed52
yuuvns2@gmail.com <yuuvns2@gmail.com>
2026-09-15 19:43:27 +0700

    lab1

--- lab1.js
+++ lab1.js
@@ -1,10 +1,10 @@
-console.log("day la lab 1");
-// PHP ; $_tenbien
-// JS let, const
-// var: ko dung var
-let name = "hoadv";
-name = "hoadv21";
-const age = 36;
+let name = "Ngô Văn Ngọc Ánh";
+let age = 22;
 
-console.log("Ten toi la: ", name);
-console.log("Tuoi cua toi la:", age);
+const address = "Hà Nội";
+const isStudent = true;
+
+console.log("Họ tên:", name);
+console.log("Tuổi:", age);
+console.log("Địa chỉ:", address);
+console.log("Sinh viên:", isStudent);
