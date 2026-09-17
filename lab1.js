9fa53b73a8305be2d42939efb0314d5658657540
yuuvns2@gmail.com <yuuvns2@gmail.com>
2026-09-17 15:35:53 +0700

    lab1_buoi2

--- lab1.js
+++ lab1.js
@@ -1,17 +1,36 @@
 let name = "Ngô Văn Ngọc Ánh";
-let age = 22;
+let tuoi = 22;
 
 const address = "Hà Nội";
 const isStudent = true;
 
 console.log("Họ tên:", name);
-console.log("Tuổi:", age);
+console.log("Tuổi:", tuoi);
 console.log("Địa chỉ:", address);
 console.log("Sinh viên:", isStudent);
 
-// Ham
-function sayHello(){
-    console.log("Xin chào");
-    return 10;
+
+console.log("Bài 2:");
+let age = 20;
+console.log(age);
+age = 21;
+console.log(age);
+
+console.log("Bài 3:");
+const a = 10;
+const b = 5;
+
+console.log("Tổng:", a + b);
+console.log("Hiệu:", a - b);
+console.log("Tích:", a * b);
+console.log("Thương:", a / b);
+
+console.log("Bài 4:");
+function sayHello(name) {
+    console.log("Xin chào " + name);
 }
-sayHello();
+
+sayHello("An");
+sayHello("Bình");
+sayHello("Chiến");
+
